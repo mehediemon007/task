5433a104679a10a51429e046bc701e6819f9bba9
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2023-02-19 02:33:41 +0600

    ff

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
-import axios from 'axios';
-import {API_BASE_URL} from "./constants";
+// import axios from 'axios';
+// import {API_BASE_URL} from "./constants";
 
 // axios.defaults.baseURL = API_BASE_URL+"/api/admin/"
 
