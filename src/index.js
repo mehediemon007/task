3023ef3cc30ec94f4a7b7bce572395fbdd31903d
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2023-02-19 03:14:49 +0600

    deploy

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
-// import axios from 'axios';
-// import {API_BASE_URL} from "./constants";
+import axios from 'axios';
+import {API_BASE_URL} from "./constants";
 
-// axios.defaults.baseURL = API_BASE_URL+"/api/admin/"
+axios.defaults.baseURL = API_BASE_URL+"/api/admin/"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -2,42 +2,42 @@ import axios from "axios";
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 
-export const loginData = async (data) => {
-  return await axios.post(`/api/admin/login`, data).then((res) => {
-      return res.data;
-  });
-}
-
-export const fetchLeads = async () => {
-  return await axios.post(`/api/admin/lead/list`).then((res) => {
-      return res.data;
-  });
-}
-
-export const filterLeads = async (data) => {
-  return await axios.post(`api/admin/lead/list`, data).then((res) => {
-      return res.data;
-  });
-}
-
-
 // export const loginData = async (data) => {
-//   return await axios.post("/login", data).then((res) => {
+//   return await axios.post(`/api/admin/login`, data).then((res) => {
 //       return res.data;
 //   });
 // }
 
 // export const fetchLeads = async () => {
-//   return await axios.post("/lead/list").then((res) => {
+//   return await axios.post(`/api/admin/lead/list`).then((res) => {
 //       return res.data;
 //   });
 // }
 
 // export const filterLeads = async (data) => {
-//   return await axios.post("/lead/list", data).then((res) => {
+//   return await axios.post(`api/admin/lead/list`, data).then((res) => {
 //       return res.data;
 //   });
 // }
+
+
+export const loginData = async (data) => {
+  return await axios.post("/login", data).then((res) => {
+      return res.data;
+  });
+}
+
+export const fetchLeads = async () => {
+  return await axios.post("/lead/list").then((res) => {
+      return res.data;
+  });
+}
+
+export const filterLeads = async (data) => {
+  return await axios.post("/lead/list", data).then((res) => {
+      return res.data;
+  });
+}
 
 export const fetchStatus = async () => {
   return await axios.get("/base/lead-status",).then((res) => {
