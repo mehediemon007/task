79626e62f59e2f2747955ad9ed9fcc62470eeb99
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2023-02-19 02:27:32 +0600

    ff

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from 'axios';
 import {API_BASE_URL} from "./constants";
 
-// axios.defaults.baseURL = API_BASE_URL+"/api/admin/";
+// axios.defaults.baseURL = API_BASE_URL+"/api/admin/"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
