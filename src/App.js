82557daed09264eda33545598f4d48d0fd050e49
Mehedi Hasan Emon <34378422+mehediemon007@users.noreply.github.com>
2023-02-18 09:51:19 +0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import 'font-awesome/css/font-awesome.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="text-center"></div>
     </div>
   );
 }
